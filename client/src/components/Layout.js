a9ca5a9d7b18a28d5cd95c49a983f9bf3367b1e5
bibek <bibekbistey>
2023-07-16 12:31:25 +0545

    responsive

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -212,8 +212,123 @@
 // export default Layout;
 
 
+// import React from "react";
+// import { adminMenu, userMenu } from "./../Data/data";
+
+// import { Badge, message } from "antd";
+// import { useSelector } from "react-redux";
+// import { Link, useLocation, useNavigate } from "react-router-dom";
+
+// const Layout = ({ children }) => {
+//   const { user } = useSelector((state) => state.user);
+//   const location = useLocation();
+//   const navigate = useNavigate();
+
+//   // logout function
+//   const handleLogout = () => {
+//     localStorage.clear();
+//     message.success("Logout Successfully");
+//     navigate("/login");
+//   };
+
+//   // Doctor menu
+//   const doctorMenu = [
+//     {
+//       name: "Home",
+//       path: "/",
+//       icon: "fa-solid fa-house",
+//     },
+//     {
+//       name: "Appointments",
+//       path: "/doctor-appointments",
+//       icon: "fa-solid fa-list",
+//     },
+//     {
+//       name: "Profile",
+//       path: `/doctor/profile/${user?._id}`,
+//       icon: "fa-solid fa-user",
+//     },
+//   ];
+
+//   // Rendering menu list
+//   const SidebarMenu = user?.isAdmin
+//     ? adminMenu
+//     : user?.isDoctor
+//     ? doctorMenu
+//     : userMenu;
+
+//   return (
+//     // <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 min-h-screen flex">
+//     <div className="bg-transparent min-h-screen flex">
+
+//       {/* Sidebar */}
+//       <div className="w-1/5 bg shadow-md">
+//         <div className="p-4">
+//           <h6 className="text-2xl text-blue-600 font-bold">MediBook</h6>
+//           <hr className="my-4" />
+//           <div className="space-y-2">
+//             {SidebarMenu.map((menu) => {
+//               const isActive = location.pathname === menu.path;
+//               return (
+//                 <div
+//                   className={`flex items-center p-2 rounded-lg ${
+//                     isActive ? "bg-blue-200" : ""
+//                   }`}
+//                 >
+//                   <i className={`${menu.icon} mr-2 text-blue-600`}></i>
+//                   <Link
+//                     to={menu.path}
+//                     className={`text-blue-600 ${
+//                       isActive ? "font-bold" : ""
+//                     }`}
+//                   >
+//                     {menu.name}
+//                   </Link>
+//                 </div>
+//               );
+//             })}
+//             <div
+//               className="flex items-center p-2 rounded-lg"
+//               onClick={handleLogout}
+//             >
+//               <i className="fa-solid fa-right-from-bracket mr-2 text-blue-600"></i>
+//               <Link to="/login" className="text-blue-600">
+//                 Logout
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       {/* Content */}
+//       <div className="w-4/5 bg-white p-8 bg-[url('assets/background6.webp')] bg-cover">
+//         {/* Header */}
+//         <div className="flex justify-end items-center mb-4">
+//           <div className="flex items-center">
+//             <Badge
+//               count={user && user.notifcation.length}
+//               onClick={() => {
+//                 navigate("/notification");
+//               }}
+//             >
+//               <i className="fa-solid fa-bell text-blue-600"></i>
+//             </Badge>
+//             <Link to="/profile" className="text-blue-600 ml-2 font-bold">
+//               {user?.name}
+//             </Link>
+//           </div>
+//         </div>
+//         {/* Body */}
+//         <div>{children}</div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+
+
 import React from "react";
-import "../styles/LayoutStyles.css";
 import { adminMenu, userMenu } from "./../Data/data";
 
 import { Badge, message } from "antd";
@@ -259,9 +374,7 @@ const Layout = ({ children }) => {
     : userMenu;
 
   return (
-    // <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 min-h-screen flex">
-    <div className="bg-transparent min-h-screen flex">
-
+    <div className="min-h-screen flex">
       {/* Sidebar */}
       <div className="w-1/5 bg shadow-md">
         <div className="p-4">
@@ -272,6 +385,7 @@ const Layout = ({ children }) => {
               const isActive = location.pathname === menu.path;
               return (
                 <div
+                  key={menu.path}
                   className={`flex items-center p-2 rounded-lg ${
                     isActive ? "bg-blue-200" : ""
                   }`}
@@ -279,9 +393,9 @@ const Layout = ({ children }) => {
                   <i className={`${menu.icon} mr-2 text-blue-600`}></i>
                   <Link
                     to={menu.path}
-                    className={`text-blue-600 ${
+                    className={`${
                       isActive ? "font-bold" : ""
-                    }`}
+                    } hidden md:flex items-center text-blue-600`}
                   >
                     {menu.name}
                   </Link>
@@ -294,38 +408,40 @@ const Layout = ({ children }) => {
             >
               <i className="fa-solid fa-right-from-bracket mr-2 text-blue-600"></i>
               <Link to="/login" className="text-blue-600">
-                Logout
+                <span className={`${user ? "hidden md:inline" : "inline"}`}>
+                  Logout
+                </span>
               </Link>
             </div>
           </div>
         </div>
       </div>
+
       {/* Content */}
-      <div className="w-4/5 bg-white p-8 bg-[url('assets/background6.webp')] bg-cover">
-        {/* Header */}
-        <div className="flex justify-end items-center mb-4">
-          <div className="flex items-center">
-            <Badge
-              count={user && user.notifcation.length}
-              onClick={() => {
-                navigate("/notification");
-              }}
-            >
-              <i className="fa-solid fa-bell text-blue-600"></i>
-            </Badge>
-            <Link to="/profile" className="text-blue-600 ml-2 font-bold">
-              {user?.name}
-            </Link>
-          </div>
-        </div>
-        {/* Body */}
-        <div>{children}</div>
-      </div>
+      <div className="w-4/5 bg-white p-8 bg-[url('assets/background6.webp')] bg-cover bg-center bg-no-repeat">
+  {/* Header */}
+  <div className="flex justify-end items-center mb-4">
+    <div className="flex items-center">
+      <Badge
+        count={user && user.notifcation.length}
+        onClick={() => {
+          navigate("/notification");
+        }}
+      >
+        <i className="fa-solid fa-bell text-blue-600"></i>
+      </Badge>
+      <Link to="/profile" className="text-blue-600 ml-2 font-bold">
+        {user?.name}
+      </Link>
+    </div>
+  </div>
+
+  {/* Body */}
+  <div>{children}</div>
+</div>
+
     </div>
   );
 };
 
 export default Layout;
-
-
-
--- client/src/pages/admin/Doctors.js
+++ client/src/pages/admin/Doctors.js
@@ -96,6 +96,109 @@
 
 
 
+// import { Table, message } from "antd";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import Layout from "./../../components/Layout";
+
+// const Doctors = () => {
+//   const [doctors, setDoctors] = useState([]);
+
+//   // getUsers
+//   const getDoctors = async () => {
+//     try {
+//       const res = await axios.get("/api/v1/admin/getAllDoctors", {
+//         headers: {
+//           Authorization: `Bearer ${localStorage.getItem("token")}`,
+//         },
+//       });
+//       if (res.data.success) {
+//         setDoctors(res.data.data);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   // handle account
+//   const handleAccountStatus = async (record, status) => {
+//     try {
+//       const res = await axios.post(
+//         "/api/v1/admin/changeAccountStatus",
+//         { doctorId: record._id, userId: record.userId, status: status },
+//         {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem("token")}`,
+//           },
+//         }
+//       );
+//       if (res.data.success) {
+//         message.success(res.data.message);
+//         window.location.reload();
+//       }
+//     } catch (error) {
+//       message.error("Something Went Wrong");
+//     }
+//   };
+
+//   useEffect(() => {
+//     getDoctors();
+//   }, []);
+
+//   const columns = [
+//     {
+//       title: "Name",
+//       dataIndex: "name",
+//       render: (text, record) => (
+//         <span>
+//           {record.firstName} {record.lastName}
+//         </span>
+//       ),
+//     },
+//     {
+//       title: "Status",
+//       dataIndex: "status",
+//     },
+//     {
+//       title: "Phone",
+//       dataIndex: "phone",
+//     },
+//     {
+//       title: "Actions",
+//       dataIndex: "actions",
+//       render: (text, record) => (
+//         <div className="flex">
+//           {record.status === "pending" ? (
+//             <button
+//               className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+//               onClick={() => handleAccountStatus(record, "approved")}
+//             >
+//               Approve
+//             </button>
+//           ) : (
+//             <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2">
+//               Reject
+//             </button>
+//           )}
+//         </div>
+//       ),
+//     },
+//   ];
+
+//   return (
+//     <Layout>
+//       <h1 className="text-center m-3 text-3xl font-bold">All Doctors</h1>
+//       <Table columns={columns} dataSource={doctors} />
+//     </Layout>
+//   );
+// };
+
+// export default Doctors;
+
+
+
+
+
 import { Table, message } from "antd";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -104,7 +207,6 @@ import Layout from "./../../components/Layout";
 const Doctors = () => {
   const [doctors, setDoctors] = useState([]);
 
-  // getUsers
   const getDoctors = async () => {
     try {
       const res = await axios.get("/api/v1/admin/getAllDoctors", {
@@ -120,7 +222,6 @@ const Doctors = () => {
     }
   };
 
-  // handle account
   const handleAccountStatus = async (record, status) => {
     try {
       const res = await axios.post(
@@ -167,16 +268,19 @@ const Doctors = () => {
       title: "Actions",
       dataIndex: "actions",
       render: (text, record) => (
-        <div className="flex">
+        <div className="flex flex-wrap">
           {record.status === "pending" ? (
             <button
-              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2"
+              className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded mr-2 mb-2 md:mb-0"
               onClick={() => handleAccountStatus(record, "approved")}
             >
               Approve
             </button>
           ) : (
-            <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2">
+            <button
+              className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded mr-2 mb-2 md:mb-0"
+              onClick={() => handleAccountStatus(record, "rejected")}
+            >
               Reject
             </button>
           )}
@@ -188,7 +292,14 @@ const Doctors = () => {
   return (
     <Layout>
       <h1 className="text-center m-3 text-3xl font-bold">All Doctors</h1>
-      <Table columns={columns} dataSource={doctors} />
+      <div className="overflow-x-auto">
+        <Table
+          className="w-full table-auto"
+          columns={columns}
+          dataSource={doctors}
+          pagination={false}
+        />
+      </div>
     </Layout>
   );
 };
